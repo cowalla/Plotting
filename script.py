a7f607632d08cdd1c0f2e7a01156e9a2530d2423
Connor Wallace <connor.wallace@urbanairship.com>
2014-12-10 14:45:14 -0800

    best version

--- script.py
+++ script.py
@@ -2,20 +2,16 @@ from matrix import *
 import matplotlib.pyplot as plt
 
 matrices = []
-maximum = 150
-sz = 150
+maximum = 200
+sz = 200
+
 for index in range(0, maximum + 1):
     alpha = float(index)/float(maximum)
-    matrices.append(PerturbedMatrix(g=0.1, a=float(alpha), size=sz))
+    matrices.append(PerturbedMatrix(g=0.0, a=float(alpha), size=sz))
 
 plt.figure()
 for mat in matrices:
-    plt.plot(mat.eigenvalues, mat.alphas, '.')
+    plt.plot(mat.eigenvalues, mat.alphas, '.', color='b', markersize=1.8)
 
 plt.axis([-4.5, 4.5, 0, 1])
 plt.show()
-
-
-# plt.plot(a.eigenvalues, a.alphas, '.')
-# plt.ylabel('some numbers')
-# plt.show()
